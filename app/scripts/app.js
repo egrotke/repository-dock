5ee13d855f744c586170be2210ec7b148b592cfe
Eric Grotke <egrotke@gmail.com>
2016-01-01 11:54:01 -0800

    Better pagination

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,29 +2,29 @@
 /* App Module */
 
 var repoApp = angular.module('repoApp', [
-  'ngRoute',
-  'repoControllers'
+    'ngRoute',
+    'repoControllers'
 ]);
 
 repoApp.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider.
-      when('/', {
-         templateUrl: 'views/main.html',
-         controller: 'MainCtrl'
-      }).when('/show', {
-         templateUrl: 'views/show.html',
-         controller: 'ShowCtrl'
-});
-$locationProvider.html5Mode(false).hashPrefix('!');
-}]);
+    function($routeProvider, $locationProvider) {
+        $routeProvider.
+        when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        }).when('/show', {
+            templateUrl: 'views/show.html',
+            controller: 'ShowCtrl'
+        });
+        $locationProvider.html5Mode(false).hashPrefix('!');
+    }
+]);
 
 repoApp.filter('startFrom', function() {
     return function(input, start) {
         start = +start; //parse to int
-        if(input){
-          console.log("Start from: " + start);
-          return input.slice(start);
+        if (input) {
+            return input.slice(start);
         }
     };
 });
--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -4,15 +4,19 @@
 var repoControllers =
     angular.module('repoControllers', []);
 
-repoControllers.controller('MainCtrl', ['$scope', '$http',
-    function MainCtrl($scope, $http) {
-        $scope.message = "Hello main";
+repoControllers.controller('MainCtrl', ['$scope', '$http', '$filter',
+    function MainCtrl($scope, $http, $filter) {
         $scope.accountFilter = "";
         $scope.accounts = [];
         $scope.currentPage = 1;
         $scope.maxItemsPerPage = 8;
         $scope.numberOfPages = function() {
-            return Math.ceil($scope.repos.length / $scope.maxItemsPerPage);
+            if($scope.filteredRepos){
+               return Math.ceil($scope.filteredRepos.length / $scope.maxItemsPerPage);
+           } 
+        };
+        $scope.newFilter = function() {
+            $scope.currentPage = 1;
         };
         $scope.getNumberArray = function(num) {
             return new Array(num);
@@ -22,18 +26,16 @@ repoControllers.controller('MainCtrl', ['$scope', '$http',
         };
         $scope.incrementCurrentPage = function() {
             var numPages = $scope.numberOfPages();
-            console.log("numRepos: " + $scope.repos.length);
             $scope.currentPage = ($scope.currentPage < numPages) ? $scope.currentPage + 1 : numPages;
         };
-        $scope.updatePageNumber = function(index){
-        	$scope.currentPage = index+1;
+        $scope.updatePageNumber = function(index) {
+            $scope.currentPage = index + 1;
         };
         $http.get('json/repos.json').success(function(data) {
             $scope.repos = data.repos;
 
             $scope.repos.forEach(function(item) {
                 if ($scope.accounts.indexOf(item.account) === -1) {
-                    console.log(item.account);
                     $scope.accounts.push(item.account);
                 }
             });
@@ -42,8 +44,3 @@ repoControllers.controller('MainCtrl', ['$scope', '$http',
     }
 ]);
 
-repoControllers.controller('ShowCtrl', ['$scope',
-    function ShowCtrl($scope) {
-        $scope.message = "Show The World";
-    }
-]);
